408dbe1105c750e3855db7fe336ce2497eb61c0a
Jeongmmin <seo88kr@hanmail.net>
2021-09-06 00:44:09 +0900

    fix. 전화번호 부분 안보이도록 수정

--- src/store/about.js
+++ src/store/about.js
@@ -4,7 +4,7 @@ export default {
         name: 'JeongMin',
         email: 'jmindev0808@gmail.com',
         blog: 'https://min88.tistory.com/',
-        phone: '+82-10-2770-2656',
+        phone: '+82-10-2770-0000',
         image: 'https://i.redd.it/wbywej1l8lg21.jpg'
     })
 }
